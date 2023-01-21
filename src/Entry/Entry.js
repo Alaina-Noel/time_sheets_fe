d18fc3bcc5bce6b368469fec5e87284fa0d8e5b8
Alaina Kneiling <alaina.noel.8@gmail.com>
2023-01-21 13:07:45 -0700

    added styling for money amount

--- src/Entry/Entry.js
+++ src/Entry/Entry.js
@@ -2,13 +2,20 @@ import React from "react";
 import './Entry.css'
 
 const Entry = ({companyName, projectName, totalHours, billableHours, billableAmount }) => {
+
+const numberFormat = new Intl.NumberFormat('en-US', { 
+  style: 'currency', 
+  currency: 'USD',
+});
+
+const billableAmountDisplayed = numberFormat.format(billableAmount);
   return(
     <div className='entry-card'>
         <div className='companyName' style={{gridColumn: "1/2"}}>{companyName}</div>
         <div className='projectName' style={{gridColumn: "2/3"}}>{projectName}</div>
-        <div className='totalHours' style={{gridColumn: "3/4"}}>{Math.round(totalHours)}</div>
-        <div className='billableHours' style={{gridColumn: "4/5"}}>{Math.round(billableHours, 1)}</div>
-        <div className='billableAmount' style={{gridColumn: "5/6"}}>${Math.round(billableAmount, 1)}</div>
+        <div className='totalHours' style={{gridColumn: "3/4"}}>{totalHours.toFixed(2)}</div>
+        <div className='billableHours' style={{gridColumn: "4/5"}}>{billableHours.toFixed(2)}</div>
+        <div className='billableAmount' style={{gridColumn: "5/6"}}>{billableAmountDisplayed}</div>
     </div>
   )
 }
