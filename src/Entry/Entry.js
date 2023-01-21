2b36b93bdad6dd55e5fafa88087a703cf8d5e80e
Alaina Kneiling <alaina.noel.8@gmail.com>
2023-01-21 15:22:21 -0700

    switched order of elements on the page

--- src/Entry/Entry.js
+++ src/Entry/Entry.js
@@ -11,8 +11,8 @@ const numberFormat = new Intl.NumberFormat('en-US', {
 const billableAmountDisplayed = numberFormat.format(billableAmount);
   return(
     <div className='entry-card'>
-        <div className='companyName' style={{gridColumn: "1/2"}}>{companyName}</div>
-        <div className='projectName' style={{gridColumn: "2/3"}}>{projectName}</div>
+        <div className='projectName' style={{gridColumn: "1/2"}}>{projectName}</div>
+        <div className='companyName' style={{gridColumn: "2/3"}}>{companyName}</div>
         <div className='totalHours' style={{gridColumn: "3/4"}}>{totalHours.toFixed(2)}</div>
         <div className='billableHours' style={{gridColumn: "4/5"}}>{billableHours.toFixed(2)}</div>
         <div className='billableAmount' style={{gridColumn: "5/6"}}>{billableAmountDisplayed}</div>
