def6b60eb81b0eae9b990391925abbf7ec4ec08f
Alaina Kneiling <alaina.noel.8@gmail.com>
2023-01-22 09:21:43 -0700

    styling follows photo

--- src/Entry/Entry.js
+++ src/Entry/Entry.js
@@ -14,7 +14,7 @@ const billableAmountDisplayed = numberFormat.format(billableAmount);
         <div className='projectName' style={{gridColumn: "1/2"}}>{projectName}</div>
         <div className='companyName' style={{gridColumn: "2/3"}}>{companyName}</div>
         <div className='totalHours' style={{gridColumn: "3/4"}}>{totalHours.toFixed(2)}</div>
-        <div className='billableHours' style={{gridColumn: "4/5"}}>{billableHours.toFixed(2)} ({Math.round((billableHours / totalHours) * 100)}%)</div>
+        <div className='billableHours' style={{gridColumn: "4/5"}}><strong>{billableHours.toFixed(2)} </strong>({Math.round((billableHours / totalHours) * 100)}%)</div>
         <div className='billableAmount' style={{gridColumn: "5/6"}}>{billableAmountDisplayed}</div>
     </div>
   )
